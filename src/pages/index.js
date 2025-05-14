b30b34852baffe68098522042a0f7c390643bf3b
Tugsuu <erdenetugsuu145@gmail.com>
2025-05-14 11:17:50 +0800

    mongol hel deer bolgow

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
                 width={20}
                 height={20}
               />
-              Deploy end hiine
+              Деплое энд хийнэ.
             </a>
             <a
               href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
